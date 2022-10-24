ae07060ee0ba81b87a2183ef7bff2c1ad36d29e2
Alexander Evans <kingevans22202@gmail.com>
2022-10-24 15:26:34 -0700

    complete

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ words = input('Enter the list: ').split()
 are = ['a', 'r', 'e']
 idxlst = []
 
-# ******************************
-# Make your Code
-# ******************************
+for i in range(len(words)):
+    flagA = False
+    flagR = False
+    FlagE = False
+    if 'a' in words[i]:
+        flagA = True
+    if 'r' in words[i]:
+        flagR = True
+    if 'e' in words[i]:
+        flagE = True
+
+    if flagA and flagR and flagE:
+        idxlst.append(words[i])      
 
 # print the words that has 'a', 'r', 'e' in sequence
 
+print (idxlst)
